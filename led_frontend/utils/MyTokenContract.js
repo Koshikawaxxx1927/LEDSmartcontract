629f06d2fb3ddb5126c82dc3e1be58c47ebedd6b
Koshikawaxxx1927 <ebi1927@icloud.com>
2023-08-01 14:29:01 +0900

    http.corsdomain is added in geth command in docker-compose file

--- led_frontend/utils/MyTokenContract.js
+++ led_frontend/utils/MyTokenContract.js
@@ -3,6 +3,9 @@ import { ethers } from "ethers";
 import MyToken from "../../led_smartcontract/data/MyToken.json";
 import LEDContract from "../../led_smartcontract/artifacts/contracts/LED.sol/LED.json";
 
+require("dotenv").config();
+const url = process.env.HARDHAT_RPC_URL;
+console.log("URL -> ", url);
 export const provider = new ethers.providers.JsonRpcProvider(
   process.env.HARDHAT_RPC_URL
 );
