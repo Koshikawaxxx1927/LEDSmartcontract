c8740378b9f6c7321eed6171a533161f59c1a927
Koshikawaxxx1927 <ebi1927@icloud.com>
2023-08-03 10:55:36 +0900

    I use precompiled tools in prysm.

--- node2/setup.sh
+++ node2/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # prysm install & build
-git clone https://github.com/prysmaticlabs/prysm && cd prysm
-go build -o=../beacon-chain ./cmd/beacon-chain
-go build -o=../validator ./cmd/validator
-go build -o=../prysmctl ./cmd/prysmctl
-cd ..
+git clone https://github.com/Koshikawaxxx1927/LEDSmartcontract
+wget -P ./prysm/cmd https://github.com/prysmaticlabs/prysm/releases/download/v4.0.7/beacon-chain-v4.0.7-linux-arm64
+wget -P ./prysm/cmd https://github.com/prysmaticlabs/prysm/releases/download/v4.0.7/validator-v4.0.7-linux-arm64
+wget -P ./prysm/cmd https://github.com/prysmaticlabs/prysm/releases/download/v4.0.7/prysmctl-v4.0.7-linux-arm64
+
+cp ./prysm/cmd/beacon-chain-v4.0.7-linux-arm64 ./beacon-chain
+cp ./prysm/cmd/validator-v4.0.7-linux-arm64 ./validator
+cp ./prysm/cmd/prysmctl-v4.0.7-linux-arm64 ./prysmctl
+
 
 # geth install & build
 git clone https://github.com/ethereum/go-ethereum && cd go-ethereum
@@ -15,4 +19,3 @@ cd ..
 
 ./geth --datadir=gethdata init genesis.json
 ./geth --datadir=gethdata account import secret.json
-
