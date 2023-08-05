ae13eb2b2eef2a514dec5e5ba011c9a107bc1dad
Koshikawaxxx1927 <ebi1927@icloud.com>
2023-08-05 10:23:35 +0900

    Fix typo

--- node-x64/run-geth.sh
+++ node-x64/run-geth.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./geth --http --http.api "eth,engine" --datadir=gethdata --log.file="gethdata/geth.log" --authrpc.jwtsecret=gethdata/geth/jetsecret --allow-insecure-unlock --unlock="0x123463a4b065722e99115d6c222f267d9cabb524" --password="" --nodiscover console --syncmode=full --allow-insecure-unlock
+./geth --http --http.api "eth,engine" --datadir=gethdata --log.file="gethdata/geth.log" --authrpc.jwtsecret=gethdata/geth/jwtsecret --allow-insecure-unlock --unlock="0x123463a4b065722e99115d6c222f267d9cabb524" --password="" --nodiscover console --syncmode=full --allow-insecure-unlock
